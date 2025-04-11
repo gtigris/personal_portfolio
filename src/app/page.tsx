8a8bd20226403452c68beca34785885cb45f9c43
gjtigris <g.tigris@gxp.co.jp>
2025-04-11 19:02:52 +0900

    feature: fixed GImage contain and cover to have sizes full and auto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import { GButton } from "@/components/ui/gButton";
-import GHeader from "@/components/ui/Header/GHeader";
+import { GButton } from '@/components/ui/gButton';
+import GHeader from '@/components/ui/Header/GHeader';
 
-import GIcon from "@/components/ui/gIcon";
-import { cn } from "@/lib/utils";
-import { GCarousel } from "@/components/ui/Carousel/GCarousel";
-import GMainVisual from "@/components/ui/MainVisual/GMainVisual";
-import GCard from "@/components/ui/Card/GCard";
-import GImage from "@/components/ui/Image/GImage";
+import GIcon from '@/components/ui/gIcon';
+import { cn } from '@/lib/utils';
+import { GCarousel } from '@/components/ui/Carousel/GCarousel';
+import GMainVisual from '@/components/ui/MainVisual/GMainVisual';
+import GCard from '@/components/ui/Card/GCard';
+import GImage from '@/components/ui/Image/GImage';
 
 export default function HomePage() {
   return (
     <>
-      <div className={cn("bg-slate-200 w-dvw min-h-dvh")}>
+      <div className={cn('bg-slate-200 w-dvw min-h-dvh')}>
         <div>
-          <h1 className={cn("text-center text-7xl font-bold")}>Modules</h1>
+          <h1 className={cn('text-center text-7xl font-bold')}>Modules</h1>
           <h2>GButton</h2>
           <GButton>Default</GButton>
           <GButton variant="destructive">Destructive</GButton>
@@ -22,47 +22,46 @@ export default function HomePage() {
           <GButton variant="ghost">Ghost</GButton>
           <GButton variant="link">Link</GButton>
         </div>
-        <div className={cn(" mt-4 border-t-2 border-slate-700")}>
+        <div className={cn(' mt-4 border-t-2 border-slate-700')}>
           <h2>GIcon</h2>
           <GIcon type="chevron-right"></GIcon>
           <GIcon type="arrow-right"></GIcon>
           <GIcon type="trash"></GIcon>
           <GIcon type="star"></GIcon>
         </div>
-        <div className={cn(" mt-4 border-t-2 border-slate-700")}>
+        <div className={cn(' mt-4 border-t-2 border-slate-700')}>
           <h2>GHeader</h2>
           <GHeader />
         </div>
-        <div className={cn("mt-4 border-t-2 border-slate-700")}>
+        <div className={cn('mt-4 border-t-2 border-slate-700')}>
           <h2>GCarousel</h2>
-          <div className={cn("flex justify-center align-middle")}>
+          <div className={cn('flex justify-center align-middle')}>
             <GCarousel />
           </div>
         </div>
-        <div className={cn("mt-4 border-t-2 border-slate-700")}>
+        <div className={cn('mt-4 border-t-2 border-slate-700')}>
           <h2>GMainVisual</h2>
           <div>
             <GMainVisual />
           </div>
         </div>
-        <div className={cn("mt-4 border-t-2 border-slate-700")}>
+        <div className={cn('mt-4 border-t-2 border-slate-700')}>
           <h2>GCard</h2>
           <div className=" w-full bg-gray-500 flex align-middle justify-center">
             <GCard />
           </div>
         </div>
-        <div className={cn("mt-4 border-t-2 border-slate-700 ")}>
+        <div className={cn('mt-4 border-t-2 border-slate-700 ')}>
           <h2>GImage</h2>
-          <div>
+          <div className="relative h-[400px]">
             <GImage
               src="/mainVisual.jpg"
-              alt={"mainVisual"}
-              type="cover"
-              // className="h-[400px]"
+              alt={'mainVisual'}
+              objectFit="contain"
             />
           </div>
         </div>
-        <div className={cn("mt-4 border-t-2 border-slate-700 ")}>
+        <div className={cn('mt-4 border-t-2 border-slate-700 ')}>
           <h2>GComponent</h2>
           <div>GComponent Content</div>
         </div>
